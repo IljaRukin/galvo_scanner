6a79a6fd142b108e038542f96d137042814d96ae
IljaRukin <ilja.rukin@gmail.com>
2021-01-03 01:09:53 +0100

    fix video

--- test/TWI.h
+++
@@ -1,9 +0,0 @@
-//source: https://embedds.com/programming-avr-i2c-interface/
-
-void TWIInit(void);
-void TWIStart(void);
-void TWIStop(void);
-void TWIWrite(uint8_t u8data);
-uint8_t TWIReadACK(void);
-uint8_t TWIReadNACK(void);
-uint8_t TWIGetStatus(void);
